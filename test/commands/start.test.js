a02cb02d24ae24d08a6b0c51696899c6475a9daf
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2020-03-03 15:12:09 +1300

    Add test for start command (#262)
    Co-authored-by: Shazron Abdullah <shazron@gmail.com>

--- test/commands/start.test.js
+++ test/commands/start.test.js
@@ -1,2 +1,28 @@
-test('test', function () {
+const TheCommand = require('../../src/commands/start')
+
+jest.mock('simctl')
+const simctl = require('simctl')
+
+let command
+beforeEach(() => {
+  command = new TheCommand([])
+})
+
+test('start run', function () {
+  const json = fixtureJson('simctl-list.json')
+  const deviceType = 'com.apple.CoreSimulator.SimDeviceType.iPhone-SE,iOS 12.1'
+  const availbleDevice = json.devices['iOS 12.1']
+    .find(({ isAvailable, name }) => isAvailable && name === 'iPhone SE')
+
+  simctl.list = jest.fn(() => {
+    return {
+      json
+    }
+  })
+  simctl.extensions = { start: jest.fn() }
+
+  command.argv = ['--devicetypeid', deviceType]
+  return command.run().then((result) => {
+    expect(simctl.extensions.start).toHaveBeenCalledWith(availbleDevice.udid)
+  })
 })
